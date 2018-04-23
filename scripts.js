3d04a32f8f15b63c527238a4919991858adb9091
benceHornyak <sir.bence15@gmail.com>
2018-04-23 20:47:15 +0200

    Basic getting the quote, and tweeting the quote

--- scripts.js
+++ scripts.js
@@ -4,17 +4,32 @@ $(document).ready(function () {
   const quoteFooter = $("#quote-footer");
   const twitterBtn = $("#twitter");
   const newQuoteBtn = $("#newQuote");
-
+ 
   newQuoteBtn.click(function() {
     getQuote();
   });
 
+
+ // https://twitter.com/intent/tweet?text=TEXT
   function getQuote() {
+    newQuoteBtn.children().toggleClass('fa-spin');
+    newQuoteBtn.prop('disabled', true);
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?", function (json) {
-      quoteBody.html(json[0].content);
-      quoteFooter.html(json[0].title);
-    })
-  };
+
+      let quote = json[0].content;
+      let author = json[0].title;
+      quoteBody.html(quote);
+      quoteFooter.html(author);
+      newQuoteBtn.children().toggleClass('fa-spin');
+      newQuoteBtn.prop('disabled', false);
+      
+      quote = quote.replace(/(<([^>]+)>)/ig , "");
+      quote = quote.replace(/&#8217;/ig, "'");
+
+      twitterBtn.attr("href", 'https://twitter.com/intent/tweet?text="' + quote  + '" -- "' + author);
+
+    });
+  }
 
   getQuote();
 });
